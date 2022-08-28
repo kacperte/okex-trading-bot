ac2b7d3fd689dc8cdbd4d95770b344b21a12a317
Kacper <k.trzepiecinski@gmail.com>
2022-08-29 00:28:38 +0200

    simplify code

--- draft.py
+++ draft.py
@@ -1,5 +1,6 @@
-import requests
+# import requests
+#
+# url = "https://www.okex.com"
+# ticker = requests.get(url+'/api/v5/market/books?instId=BTC-USD-SWAP&sz=10').json()['data']
+# print(ticker)
 
-url = "https://www.okex.com"
-ticker = requests.get(url+'/api/v5/market/books?instId=BTC-USD-SWAP&sz=10').json()['data']
-print(ticker)
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import requests
+from test_json import test_json_1
+from test_json import test_json_2
+
 
 url = "https://www.okx.com/priapi/v5/rubik/app/public/new-coin-rank?t=1661446882241&type=USDC"
 input_a = requests.get(url).json()['data'][0]['online']
 input_b = len(input_a)
-
-
-def raise_new_coin(a, b):
-    a = [tuple(sorted(d.items())) for d in a]
-    b = [tuple(sorted(d.items())) for d in b]
-    return [dict(kvs) for kvs in set(a).difference(b)]
 
 
 def new_coin_alert(list_of_coins: list, n_of_coins: int):
@@ -16,7 +13,7 @@ def new_coin_alert(list_of_coins: list, n_of_coins: int):
         new_list_of_coins = requests.get(url).json()['data'][0]['online']
         new_n_of_coins = len(requests.get(url).json()['data'][0]['online'])
         if new_n_of_coins > n_of_coins:
-            new_coin = raise_new_coin(new_list_of_coins, list_of_coins)
+            new_coin = [x for x in new_list_of_coins if x not in list_of_coins]
             # function to call a traiding bot
             print("NEW COIN ALERT")
             new_coin_alert(new_list_of_coins, new_n_of_coins)
